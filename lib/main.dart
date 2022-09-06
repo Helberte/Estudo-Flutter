0683cd8967bac5ad526d828146471a9f0e0a6673
helberte <helberteads@gmail.com>
2022-09-05 20:20:05 -0400

    Criando app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
-import 'package:flutter/material.dart'; // trás todos os widget do material design
+import 'package:flutter/material.dart'; 
+import 'app/app_widget.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.red,
-        child: const Text("Olá mundo"),
-      ),
-    );
-  }
+  runApp(const AppWidget());
 }
